846960d6696d3af75a67f08acace1643705d3b65
Tomas Alvarez <tomasalvarez@Taloss-Mac-mini.local>
2019-08-12 17:09:23 -0500

    avances'

--- HTMLtrainning/CSSUdemyTrainning/Scripts/shared.js
+++ HTMLtrainning/CSSUdemyTrainning/Scripts/shared.js
@@ -3,7 +3,8 @@ var modal = document.querySelector('.modal');
 var selectPlanButtons = document.querySelectorAll('.plan button');
 var buttonno = document.querySelector('.modal__action--negative');
 var toggleButton = document.querySelector('.toggle-button');
-var mobileNav = document.querySelector('.mobile-nav')
+var mobileNav = document.querySelector('.mobile-nav');
+var ctaButton = document.querySelector(".main-nav__item--cta");
 
 for (var i = 0;i<selectPlanButtons.length;i++){
   selectPlanButtons[i].addEventListener('click',function(){
@@ -24,11 +25,23 @@ backdrop.addEventListener('click',function(){
 function cerrar(){
   if(modal){
     modal.classList.remove('open');
-  }  
+  }
   backdrop.classList.remove('open');
 };
 
 toggleButton.addEventListener('click',function(){
   mobileNav.classList.add('open');
   backdrop.classList.add('open');
+});
+
+ctaButton.addEventListener('animationstart', function(event){
+  console.log('Animation Ended ', event);
+});
+
+ctaButton.addEventListener('animationend', function(event){
+  console.log('Animation Started ', event);
+});
+
+ctaButton.addEventListener('animationiteration', function(event){
+  console.log('Animation iteration ', event);
 });
